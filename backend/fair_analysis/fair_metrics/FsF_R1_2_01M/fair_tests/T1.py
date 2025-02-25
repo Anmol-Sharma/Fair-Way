2d81e74815fa4e4e5bde27dc2e64dc05801f80b0
Anmol Sharma <anmolsharma010@gmail.com>
2025-02-25 10:22:27 +0100

    added the Metric FsF-R1.2-01M

---
+++ backend/fair_analysis/fair_metrics/FsF_R1_2_01M/fair_tests/T1.py
@@ -0,0 +1,47 @@
+from fair_analysis.fair_metrics.TestBase import BaseTest
+from fair_analysis.fair_metrics.FsF_R1_2_01M.fair_tests.t1_samples import (
+    FEW_SHOT_SAMPLES,
+    ResponseFormat,
+)
+
+########################################
+#          DEFINE TEST OBJECT          #
+########################################
+
+
+# derive from base class and utilize
+class Test(BaseTest):
+    def __init__(
+        self,
+        name: str,
+        feedback_format: str,
+        test_main_cmd: str,
+        test_instruction: str,
+        few_shot_samples=[],
+    ):
+        super().__init__(
+            name,
+            feedback_format,
+            test_main_cmd,
+            test_instruction,
+            use_few_shot_prompting=True,
+            few_shot_samples=few_shot_samples,
+        )
+
+    def filter_chunk_results(self, chunk_results):
+        # If all empty, return false, full empty (return chunks as empty)
+        _r = []
+        for ch_res in chunk_results:
+            if len(ch_res["entities"]) == 0:
+                continue
+            _r.append(ch_res)
+        return _r
+
+
+t1 = Test(
+    name="Metadata contains provenance information about creators of the data.",
+    feedback_format=ResponseFormat,
+    test_main_cmd="Your task is to help analyze the metadata provided at the end for provenance information about data. Carefully check if metadata contains information about all the following elements:- `creator`, `contributors`, `date of curation`, `version information`, `modification date` and `source`. Return True if and only if all these elements are present except contributor (it is optional), else return false.",
+    test_instruction="Check if metadata includes provenance information about data.",
+    few_shot_samples=FEW_SHOT_SAMPLES,
+)
---
+++ backend/fair_analysis/fair_metrics/FsF_R1_2_01M/fair_tests/t1_samples.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import Dict
+
+
+class ResponseFormat(BaseModel):
+    success: bool
+    entities: Dict[str, str]
+
+
+############################################
+#    Define the few shot examples below    #
+#                                          #
+# The few shot sample LLM responses should #
+# match the Response format provided above #
+############################################
+
+FEW_SHOT_SAMPLES = [
+    {
+        "ex": """# Project: Diabetes Prediction Model ## Overview This is an open-source diabetes prediction model built using Python and TensorFlow. The model uses historical medical records to predict patient outcomes. ## Dataset * **Source**: National Institutes of Health (NIH) [Diabetes dataset](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5465115/) * **Version**: v1.0 * **License**: CC BY 4.0 ## Resources * **Funding**: Supported by the National Science Foundation (NSF) grant #1234567 * **Repository**: [GitHub repository](https://github.com/project-diabetes/diabetes-prediction-model) * **Website**: [Project website](http://diabetesprediction.org) ## Citing this project If you use this dataset or model in your research, please cite the following paper: [1] Smith et al. (2020). "Diabetes prediction using machine learning." Journal of Medical Systems. ## Acknowledgments We would like to thank the NIH for providing the diabetes dataset and the NSF for supporting this project.""",
+        "feedback": """{"success": false, "entities": {"creator": "Dr. Jane Doe", "contributors": "", "date of curation": "2024-02-25", "version information": "", "modification date": "2025-02-25", "source": "https://example.org/dataset/chemicals"}}""",
+    },
+    {
+        "ex": """{"@context":"http://schema.org","@id":"https://doi.org/10.5281/zenodo.1227121","@type":"https://schema.org/Dataset","identifier":"https://doi.org/10.5281/zenodo.1227121","name":"DEMAND: a collection of multi-channel recordings of acoustic noise in diverse environments","creator":[{"name":"Thiemann, Joachim","givenName":"Joachim","familyName":"Thiemann","affiliation":[{"@type":"Organization","name":"IRISA-CNRS"}],"@id":"https://orcid.org/0000-0002-8617-8330","@type":"Person"}],"author":[{"name":"Thiemann, Joachim","givenName":"Joachim","familyName":"Thiemann","affiliation":[{"@type":"Organization","name":"IRISA-CNRS"}],"@id":"https://orcid.org/0000-0002-8617-8330","@type":"Person"}],"publisher":{"@type":"Organization","name":"Zenodo"},"keywords":"Noise, Multichannel Audio, Microphone Array","dateCreated":"2018-04-24T10:46:21.267306+00:00","dateModified":"2024-08-02T17:16:41.704421+00:00","datePublished":"2013-06-09","inLanguage":{"alternateName":"eng","@type":"Language","name":"English"},"contentSize":"6.86 GB","size":"6.86 GB","version":"1.0","license":"https://creativecommons.org/licenses/by/4.0/legalcode","url":"https://zenodo.org/records/1227121"}""",
+        "feedback": """{"success": true, "entities": {"creator": "Joachim Thiemann", "contributors": "", "date of curation": "2018-04-24T10:46:21.267306+00:00", "version information": "1.0", "modification date": "2024-08-02T17:16:41.704421+00:00", "source": "https://doi.org/10.5281/zenodo.1227121"}}""",
+    },
+]
