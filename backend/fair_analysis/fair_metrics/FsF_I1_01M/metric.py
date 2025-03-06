31c5bd58afceffdde60d718ce6e578104817447e
Anmol Sharma <anmolsharma010@gmail.com>
2025-03-06 09:23:02 +0100

    In the new multi-metadata tests, created override the default behaviour to test the FsF-I1-01M

--- backend/fair_analysis/fair_metrics/FsF_I1_01M/metric.py
+++ backend/fair_analysis/fair_metrics/FsF_I1_01M/metric.py
@@ -1,6 +1,7 @@
 from fair_analysis.fair_metrics.MetricBase import BaseMetric
 from fair_analysis.fair_metrics.FsF_I1_01M.fair_tests.T1 import t1
 from typing import Dict
+from celery import current_task
 
 
 class Metric(BaseMetric):
@@ -16,6 +17,34 @@ class Metric(BaseMetric):
             file_type=file_type,
         )
         return t_result
+
+    def analyze_metric(self, model, metadata):
+        self.logger.info(
+            f"Analyzing Metric: {self.metric_id}-{self.name} for task: {current_task.request.id}"
+        )
+        # Perform the relevant test on each of the metadata items and combine them together
+        All_Results = {}
+        for k in metadata.keys():
+            # Perform the test on complete metadata
+            res = self.execute_tests(
+                model,
+                (metadata[k]["metadata"],),  # Single complete chunk --> full metadata
+                metadata[k]["source"],
+            )
+            name = ""
+            if k == "api":
+                name = "Harvested Metadata"
+            elif k == "embedded":
+                name = "Embedded Metadata"
+            else:
+                name = "Uploaded Metadata File"
+            All_Results[name] = res
+
+        if len(metadata.keys()) <= 1:
+            return All_Results.values()[0]
+        else:
+            combined_results = self.combine_multi_metric_results(model, All_Results)
+            return self.score_test_results(combined_results)
 
     def score_test_results(self, t_results):
         score = 0.0
